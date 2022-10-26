1f0b10a5a34f43d982b6cdabe642df40b2a4218b
Chuan-Heng Hsiao <hsiao.chuanheng@gmail.com>
2022-10-26 10:43:12 -0400

    [fix]: looks like a typo in applyFirFilterSingle

--- src/FIRFilter.cpp
+++ src/FIRFilter.cpp
@@ -26,7 +26,7 @@ std::vector<float> applyFirFilterSingle(FilterInput<float>& input) {
   auto* y = input.y;
 
   for (auto i = 0u; i < input.outputLength; ++i) {
-    y[i] = x[0] * c[0];
+    y[i] = x[i] * c[0];
     for (auto j = 1u; j < input.filterLength; ++j) {
       y[i] += x[i + j] * c[j];
     }
